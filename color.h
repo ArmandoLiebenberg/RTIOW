8eea8208a104f000591025ee76d65ce0d982b9d0
Armando Liebenberg <34469720+ArmandoLiebenberg@users.noreply.github.com>
2023-01-06 15:10:41 +1000

    Rendering image and vec3 class added

---
+++ color.h
@@ -0,0 +1,14 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+
+// color here is an alias for a vec3
+void write_color(std::ostream &out, color pixelcolor) {
+    out << static_cast<int>(255.999 * pixelcolor.x()) << ' '
+        << static_cast<int>(255.999 * pixelcolor.y()) << ' '
+        << static_cast<int>(255.999 * pixelcolor.x()) << '\n';
+}
+
+#endif
